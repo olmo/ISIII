e5c0919e711af0092aba21a972865d78a4d34acd
Javier Castillo <jcasty89@gmail.com>
2012-03-21 19:43:33 +0100

    pruebas

---
+++ Iteracion1/Implementacion/GestorBD/src/GestorConexion.java
@@ -0,0 +1,111 @@
+
+import java.sql.*;
+
+import javax.swing.table.DefaultTableModel;
+import javax.swing.table.TableModel;
+
+
+public class GestorConexion {
+	private Connection conexion;
+	private ResultSet rs;
+	private Statement st;
+	
+	public GestorConexion(){
+
+	}
+	
+	public boolean conectar(){
+		try{ 
+			   Class.forName("com.mysql.jdbc.Driver");
+			   conexion = DriverManager.getConnection("jdbc:mysql://1984.dyndns.org:3306/isiii", "isiii", "qwerty");
+			   return true;
+			   }
+			catch(Exception e){ e.printStackTrace(); return false;}
+	}
+	
+	public void desconectar(){
+		try{ 
+			   if(rs!=null){
+				   rs.close();
+			   }
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		try{ 
+			   if(st!=null){
+				   st.close();
+			   }
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		try{ 
+			   if(conexion!=null){
+				   conexion.close();
+			   }
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+	}
+	
+	public boolean InsertarDatos(String SQL){
+		try{
+			if(st.executeUpdate(SQL)==1){
+				return true;
+			}
+			
+			
+		}catch(Exception ex){
+			ex.printStackTrace();
+		}
+		return false;
+	}
+	
+	public TableModel RealizarConsulta(String SQL){
+		
+		
+		//System.out.println("Realizando consulta");
+		// Preparamos la consulta 
+		
+		 DefaultTableModel modelo = new DefaultTableModel(); //Creamos la estructura para guardar los datos(UNA TABLA)
+
+					try{ 
+						 st = conexion.createStatement(); 
+						rs = st.executeQuery (SQL);
+						
+						// Recorremos el resultado, mientras haya registros para leer, y escribimos el resultado en pantalla. 
+//						while (rs.next()) 
+//						{ 
+//							
+//						    System.out.println (rs.getString (1) + " " + rs.getString (2)+ " " + rs.getDate(3)); 
+//						}
+						 
+					        for (int i = 1; i <= rs.getMetaData().getColumnCount(); i++) {//Relleneamos la cabecera de la tabla.
+					            modelo.addColumn(rs.getMetaData().getColumnName(i));
+					        }
+					        while (rs.next()){//introducimos los datos en la Tabla
+					           Object [] tupla = new Object[rs.getMetaData().getColumnCount()];
+					           for (int i=0; i<rs.getMetaData().getColumnCount();i++)
+					              tupla[i] = rs.getObject(i+1);
+					           modelo.addRow(tupla);
+					        }
+					        
+					
+					}catch(Exception ex){
+						ex.printStackTrace();
+					}
+					
+//					System.out.println(modelo.getRowCount());
+			        return modelo;
+
+					
+					
+			        
+			        
+	}
+	
+	
+
+}
+
+
+
---
+++ Iteracion1/Implementacion/GestorBD/src/Ventanainterna.java
@@ -0,0 +1,144 @@
+import java.awt.EventQueue;
+
+import javax.swing.JInternalFrame;
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+
+import java.sql.*;
+
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JTable;
+import javax.swing.JLabel;
+import javax.swing.JSeparator;
+import javax.swing.SwingConstants;
+import java.awt.FlowLayout;
+
+
+
+
+public class Ventanainterna extends JInternalFrame {
+	
+	
+//	private Connection conexion;
+//	private ResultSet rs;
+//	private Statement st;
+	GestorDatos gd;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTable table;
+
+	/**
+	 * Create the frame.
+	 */
+	public Ventanainterna(GestorConexion gc) {
+		
+//		conexion=conexion1;
+//		rs=rs1;
+//		st=st1;
+		
+		gd=new GestorDatos(gc);
+				
+		try {
+			setSelected(true);
+			setIcon(true);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		setResizable(true);
+		setMaximizable(true);
+		setIconifiable(true);
+		
+		JSplitPane splitPane = new JSplitPane();
+		splitPane.setResizeWeight(0.5);
+		splitPane.setDividerLocation(110);
+		splitPane.setOrientation(JSplitPane.VERTICAL_SPLIT);
+		getContentPane().add(splitPane, BorderLayout.CENTER);
+		
+		JPanel panel = new JPanel();
+		splitPane.setLeftComponent(panel);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JPanel panel_2 = new JPanel();
+		panel.add(panel_2, BorderLayout.NORTH);
+		
+		JButton btnMostrarTabla = new JButton("Mostrar tabla");
+		btnMostrarTabla.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				table.setModel(gd.consultardatos());
+			}
+		});
+		panel_2.add(btnMostrarTabla);
+		
+		JPanel panel_3 = new JPanel();
+		panel.add(panel_3, BorderLayout.SOUTH);
+		
+		JLabel lblDni = new JLabel("Dni");
+		panel_3.add(lblDni);
+		
+		textField = new JTextField();
+		panel_3.add(textField);
+		textField.setColumns(10);
+		
+		JLabel label_1 = new JLabel("Nombre");
+		panel_3.add(label_1);
+		
+		textField_1 = new JTextField();
+		panel_3.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JLabel lblFechaNacyyyyddmm = new JLabel("Fecha nac.(YYYY/DD/MM)");
+		panel_3.add(lblFechaNacyyyyddmm);
+		
+		textField_2 = new JTextField();
+		panel_3.add(textField_2);
+		textField_2.setColumns(10);
+		
+		JButton button = new JButton("Insertar");
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				gd.insertardatos(textField.getText(), textField_1.getText(), textField_2.getText());
+			}
+		});
+		panel_3.add(button);
+		
+		JPanel panel_4 = new JPanel();
+		panel.add(panel_4, BorderLayout.CENTER);
+		
+		JLabel lblDni_1 = new JLabel("dni");
+		panel_4.add(lblDni_1);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		panel_4.add(textField_3);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				if(!textField_3.getText().equals("")){
+					table.setModel(gd.buscardni(textField_3.getText()));
+				}
+			}
+		});
+		panel_4.add(btnBuscar);
+		
+		JPanel panel_1 = new JPanel();
+		splitPane.setRightComponent(panel_1);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblNewLabel = new JLabel("Resultados:");
+		panel_1.add(lblNewLabel, BorderLayout.NORTH);
+		
+		table = new JTable();
+		panel_1.add(table, BorderLayout.CENTER);
+	}
+
+}
