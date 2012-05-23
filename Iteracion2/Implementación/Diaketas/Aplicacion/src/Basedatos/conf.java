5a6eaeceaf3f02ade6aeef3053d63518c5bdb7ce
Olmo Jiménez <olmojial@gmail.com>
2012-05-23 17:09:50 +0200

    Revert "añadidos multiples archivos"
    This reverts commit 354874da56e78d3339aa64b33bbb4be1a82b43f3.

--- Iteracion2/Implementación/Diaketas/Aplicacion/src/Basedatos/conf.java
+++
@@ -1,6 +0,0 @@
-package Basedatos;
-
-//DB: isiii;
-//user: root;
-//pass: qwerty;
-//DBA: 198.dyndns.org:3306;
--- Iteracion2/Implementación/Diaketas/Aplicacion/src/GestionActuacion/TipoActuacion.java
+++
@@ -1,3 +0,0 @@
-package GestionActuacion;
-
-public enum TipoActuacion {conceder_ayuda, editar_ayuda, anadir_donacion, cancelar_donacion, confirmar_donacion};
