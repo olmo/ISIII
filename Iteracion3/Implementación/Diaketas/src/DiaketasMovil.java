dff35a6009b4fa18521abedfa1f93eabc82122e8
Angel <acostela1989@gmail.com>
2012-05-23 18:17:26 +0200

    añadidas interfaces moviles y main Diaketas/src/DiaketasMovil.java

---
+++ Iteracion3/Implementación/Diaketas/src/DiaketasMovil.java
@@ -0,0 +1,11 @@
+import Visual.VentanaPrincipalMovil;
+
+public class DiaketasMovil {
+
+	public static void main(String[] args) {
+		
+		VentanaPrincipalMovil vpm=new VentanaPrincipalMovil();
+		vpm.setVisible(true);
+	}
+	
+}
